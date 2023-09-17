c442a8da94ba560f468885f245bf823b1b4fa9f8
bquan0 <powerbowen@gmail.com>
2023-09-16 20:58:07 -0500

    apply changes suggested by gonuke

--- scripts/CameraJoint.gd
+++ scripts/CameraJoint.gd
@@ -10,9 +10,6 @@ func _ready():
 	set_as_toplevel(true)
 	spring_length = 7
 
-#func _process(delta):
-#	pass
-
 func _physics_process(delta):
 	# zoom the camera in or out based on the scroll wheel
 	if Input.is_action_just_released("scroll_up"):
@@ -22,10 +19,18 @@ func _physics_process(delta):
 	spring_length = clamp(spring_length, 0, 12)
 
 func _unhandled_input(event):
-	# move the camera if the RMB is held
+	# move the camera if the RMB is held and the mouse is moving
 	if event is InputEventMouseMotion and Input.is_action_pressed("right_mouse_button"):
-		rotation_degrees.y -= event.relative.x * mouse_sensitivity
-		rotation_degrees.x = clamp(rotation_degrees.x, -90.0, 90.0)
+		# note: changing rotation_degrees.x means rotating around the x-axis, 
+		# 		which would be rotating the camera up or down. 
+		#		changing rotation_degrees.y means rotating around the y-axis,
+		#		which would be rotating the camera left or right
 		
-		rotation_degrees.x -= event.relative.y * mouse_sensitivity
+		# rotate the camera left or right
+		rotation_degrees.y -= event.relative.x * mouse_sensitivity
 		rotation_degrees.y = wrapf(rotation_degrees.y, 0.0, 360.0)
+		
+		# rotate the camera left or right
+		rotation_degrees.x -= event.relative.y * mouse_sensitivity
+		rotation_degrees.x = clamp(rotation_degrees.x, -90.0, 90.0)
+	
--- scripts/Player.gd
+++ scripts/Player.gd
@@ -8,11 +8,6 @@ var velocity = Vector3.ZERO
 func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
 func _physics_process(delta):
 	var direction = Vector3.ZERO
 	
