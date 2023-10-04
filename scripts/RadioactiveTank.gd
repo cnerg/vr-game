be179834af75f59a8ac180c928392cab7fe49fb1
bquan0 <powerbowen@gmail.com>
2023-10-03 20:43:05 -0500

    add binary search function, get_bounds function, and other optimizations

--- scripts/RadioactiveTank.gd
+++ scripts/RadioactiveTank.gd
@@ -6,9 +6,9 @@ var bounds_y = []
 var bounds_z = []
 
 # array that contains voxel centroids and radiation values
-# index 0-2 are (x,y,z) of centroid
-# index 3 is radiation value
-# index 4 is relative error
+# index 0 is a Vector3 of the centroid position
+# index 1 is radiation value
+# index 2 is relative error
 var radiation_map = []
 
 # maximum indices for the 3 axes and for radiation_map
@@ -20,6 +20,13 @@ var max_index = 0
 # index of the last voxel queried from getRadiation()
 var prev_index = -1
 
+
+func get_bounds(csv_line, scale):
+	var bounds = []
+	for i in range(0, csv_line.size()):
+		bounds.append(float(csv_line[i]) * scale); 
+	return bounds
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var csv_file = File.new()
@@ -27,21 +34,12 @@ func _ready():
 	
 	# Note: for the tank, all position data is in cm. Therefore, we divide by 
 	# 100 to get meters.
+	var scale = 1.0/100
 	
-	# get x boundaries
-	var csv_line = csv_file.get_csv_line()
-	for i in range(0, csv_line.size()):
-		bounds_x.append(float(csv_line[i]) / 100);
-		
-	# get y boundaries
-	csv_line = csv_file.get_csv_line()
-	for i in range(0, csv_line.size()):
-		bounds_y.append(float(csv_line[i]) / 100);
-		
-	# get z boundaries
-	csv_line = csv_file.get_csv_line()
-	for i in range(0, csv_line.size()):
-		bounds_z.append(float(csv_line[i]) / 100);
+	# get x,y,z boundaries
+	bounds_x = get_bounds(csv_file.get_csv_line(), scale)
+	bounds_y = get_bounds(csv_file.get_csv_line(), scale)
+	bounds_z = get_bounds(csv_file.get_csv_line(), scale)	
 		
 	# calculate max x,y,z indices
 	# (-1) there's one extra bound compared to number of spaces
@@ -51,12 +49,18 @@ func _ready():
 	
 	# get voxel data
 	while !csv_file.eof_reached(): 
-		csv_line = csv_file.get_csv_line()
+		var csv_line = csv_file.get_csv_line()
 		var voxel = []
+		var voxel_centroid = Vector3.ZERO
 		for i in range(0, csv_line.size()):
-			# first 3 values are x,y,z of centroid in cm, so divide by 100
-			if i <= 2:
-				voxel.append(float(csv_line[i]) / 100)
+			# first 3 values are x,y,z of centroid in cm, so multiply by scale
+			if i == 0:
+				voxel_centroid.x = float(csv_line[i]) * scale
+			elif i == 1:
+				voxel_centroid.y = float(csv_line[i]) * scale
+			elif i == 2:
+				voxel_centroid.z = float(csv_line[i]) * scale
+				voxel.append(voxel_centroid)
 			# next 2 values are not in cm, so take them as is
 			else:
 				voxel.append(float(csv_line[i]))
@@ -66,70 +70,78 @@ func _ready():
 	
 	# calculate max total index for radiation_map
 	max_index = radiation_map.size()
+
+
+# returns the index of the largest value in the array that is still smaller 
+# than the input value, otherwise -1
+func binary_search(array, value):
+	var lower = 0
+	var upper = array.size() - 1
+	var center = 0
+	
+	# if the input value is lower than the smallest value in the array and 
+	# larger than the biggest value in the array, it's not in the array
+	if value < array[lower] or value > array[upper]:
+		return -1
+		
+	while lower < upper:
+		center = (lower + upper) / 2
+		# if lower and upper are right next to each other and values between them,
+		# return the lower index
+		if lower + 1 == upper:
+			if value >= array[lower] and value < array[upper]:
+				return lower
+			else: 
+				return -1
+		elif value > array[center]:
+			lower = center
+		elif value < array[center]:
+			upper = center
+	
+	# default return to catch any other cases			
+	return -1 
+	
 	
 # returns the radiation value and the voxel at the input position
 func get_radiation(x, y, z):
-	# get the x_index based on input x
-	var x_index = 0
-	var rel_x = x - translation.x # position relative to this tank
-	while x_index < bounds_x.size() and rel_x >= bounds_x[x_index]:
-		x_index = x_index + 1
-	
-	# get the y_index based on input y
-	var y_index = 0
-	var rel_y = y - translation.y # position relative to this tank
-	while y_index < bounds_y.size() and rel_y >= bounds_y[y_index]:
-		y_index = y_index + 1
-	
-	# get the z_index based on input z
-	var z_index = 0
-	var rel_z = z - translation.z # position relative to this tank
-	while z_index < bounds_z.size() and rel_z >= bounds_z[z_index]:
-		z_index = z_index + 1
-		
-	# subtract 1 from indices to get the real index
-	x_index = x_index - 1
-	y_index = y_index - 1
-	z_index = z_index - 1	
-
-	var index = -1
-	# check if input position is inside the radiation map
-	# min index = 1
-	# max index = size of array
-	if x_index >= 0 and x_index < max_index_x and \
-			y_index >= 0 and y_index < max_index_y and \
-			z_index >= 0 and z_index < max_index_z:
-		index = (x_index * max_index_y * max_index_z) + (y_index * max_index_z) \
-				+ z_index
-	var value = 0
+	# initialize return values
 	var centroid = Vector3.ZERO
+	var value = 0
 	
-	# only access the array for voxel info if we are in the voxel map
-	if index >= 0 and index < max_index:
-		value = radiation_map[index][3]
-		centroid.x = radiation_map[index][0]
-		centroid.y = radiation_map[index][1]
-		centroid.z = radiation_map[index][2]
-		
-		if index != prev_index:
-			# set mesh's position to voxel's centroid 
-			$CurrentVoxel.translation.x = centroid.x
-			$CurrentVoxel.translation.y = centroid.y
-			$CurrentVoxel.translation.z = centroid.z
-			
-			# set size of mesh
-			$CurrentVoxel.scale.x = bounds_x[x_index + 1] - bounds_x[x_index]
-			$CurrentVoxel.scale.y = bounds_y[y_index + 1] - bounds_y[y_index]
-			$CurrentVoxel.scale.z = bounds_z[z_index + 1] - bounds_z[z_index]
-			
-			# update prev_index to latest index queried
-			prev_index = index
-			
-			# show the mesh
-			$CurrentVoxel.visible = true
-	else:
-		# don't show the mesh if input position isn't a voxel in the map
+	# get indices
+	var x_index = binary_search(bounds_x, x - translation.x)
+	var y_index = binary_search(bounds_y, y - translation.y)
+	var z_index = binary_search(bounds_z, z - translation.z)
+	# return early if any indices are -1
+	if x_index == -1 or y_index == -1 or z_index == -1:
 		$CurrentVoxel.visible = false
+		return [value, centroid]	
+
+	# calculate index of voxel that the player is in
+	var index = (x_index * max_index_y * max_index_z) + (y_index * max_index_z) \
+				+ z_index
+	
+	# get the voxel's centroid and value
+	centroid = radiation_map[index][0]
+	value = radiation_map[index][1]
+
+	# update the voxel mesh if we are in a different voxel
+	if index != prev_index:
+		# set mesh's position to voxel's centroid 
+		$CurrentVoxel.translation.x = centroid.x
+		$CurrentVoxel.translation.y = centroid.y
+		$CurrentVoxel.translation.z = centroid.z
+		
+		# set size of mesh
+		$CurrentVoxel.scale.x = bounds_x[x_index + 1] - bounds_x[x_index]
+		$CurrentVoxel.scale.y = bounds_y[y_index + 1] - bounds_y[y_index]
+		$CurrentVoxel.scale.z = bounds_z[z_index + 1] - bounds_z[z_index]
+		
+		# update prev_index to latest index queried
+		prev_index = index
+		
+		# show the mesh
+		$CurrentVoxel.visible = true
 		
 	# return an array with the radiation value and the voxel position
 	return [value, centroid]
